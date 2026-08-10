91adcab47343c824c96033ed6df4f523508bf89f
arlindossantos-collab <arlindossantos@gmail.com>
2026-08-10 11:44:35 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,35 @@
-const CACHE_NAME = 'eletrocalc-v1';
+const CACHE_NAME = 'eletrocalc-v2';
 const ASSETS = [
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+'./index.html',
+'./manifest.json',
+'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap',
+'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+e.waitUntil(
+caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+);
+self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+e.waitUntil(
+caches.keys().then((keys) => {
+return Promise.all(
+keys.map((key) => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+);
+})
+);
+self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+e.respondWith(
+caches.match(e.request).then((response) => response || fetch(e.request))
+);
 });
